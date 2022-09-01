fe29ac6e0cde85b2952200407d7cdc58a44abf20
Jayson <jaysonm@udel.edu>
2022-08-31 22:11:34 -0400

    fetch quotes

---
+++ RandomQuoteMachine/index.js
@@ -0,0 +1,26 @@
+function App(){
+
+    const [quotes, setQuotes] = React.useState([]);
+    const [randomQuote, setRandomQuote] = React.useState([]);
+
+    React.useEffect (() => {
+        async function fetchData(){
+            const response = await fetch("https://type.fit/api/quotes")
+            const data = await response.json();
+            setQuotes(data);
+
+            let randomIndex = Math.floor(Math.random * data.length);
+            setRandomQuote(data[randomIndex])
+        }
+        fetchData();
+    }, [])
+    return(
+        <div>Hello World
+            <div>{quotes.map(quotes =>
+               quotes.text )}
+               </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
